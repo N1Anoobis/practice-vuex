c794d3684985e0bec720201452a7a848cbf816ac
N1Anoobis <mrbond84@hotmail.com>
2021-02-08 14:24:02 +0100

    Add Procfile

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <ProductListOne v-bind:products="products" />
+    <ProductListTwo v-bind:products="products" /> -->
+    <ProductListOne />
+    <ProductListTwo />
+  </div>
+</template>
+<script>
+import ProductListOne from "./components/ProductListOne";
+import ProductListTwo from "./components/ProductListTwo";
+export default {
+  name: "app",
+  data() {
+    return {
+      // products: [
+      //   { name: "Samsung", price: 200 },
+      //   { name: "Sony", price: 300 },
+      //   { name: "Asus", price: 400 },
+      //   { name: "Apple", price: 1000 },
+      // ],
+    };
+  },
+  components: {
+    ProductListOne,
+    ProductListTwo,
+  },
+};
+</script>
+<style>
+body {
+  font-family: Ubuntu;
+  color: #555;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,45 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    products: [],
+  },
+  getters: {
+    saleProducts: (state) => {
+      var saleProducts = state.products.map((product) => {
+        return {
+          name: "**" + product.name + "**",
+          price: product.price / 2,
+        };
+      });
+      return saleProducts;
+    },
+  },
+  mutations: {
+    reducePrice: (state) => {
+      state.products.forEach((product) => {
+        product.price -= 10;
+      });
+    },
+    loadAPIdataToStore: (state, data) => {
+      state.products = data;
+    },
+  },
+  actions: {
+    async fetchProducts(context) {
+      await axios
+        .get("http://localhost:3000/products")
+        .then((response) => {
+          context.commit("loadAPIdataToStore", response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  modules: {},
+});
